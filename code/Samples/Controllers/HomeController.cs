9880d6a44e53f08111adb63d1d5725ec1f6a5eed
J. Tower <jonathantower@gmail.com>
2016-08-03 08:49:52 -0400

    Adding code and slides

---
+++ code/Samples/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Samples.DI;
+
+namespace Samples.Controllers
+{
+    public class HomeController : Controller
+    {
+	    private readonly IEmailSender _emailSender;
+
+	    public HomeController(IEmailSender emailSender)
+	    {
+		    _emailSender = emailSender;
+	    }
+
+	    public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+	        _emailSender.SendEmail("to@email.com", "subject line", "message");
+
+			return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
